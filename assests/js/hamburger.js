530bff7b58cc59c8c877bc7f35faa831d78ecc36
mingus <soeren.krumm@stud.uni-hannover.de>
2025-02-08 02:07:27 +0100

    hamburger menu works now better

--- assests/js/hamburger.js
+++ assests/js/hamburger.js
@@ -3,11 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const mobileMenu = document.getElementById("mobile-menu");
     const navList = document.querySelector(".navbar ul");
+    const navbar = document.querySelector(".navbar"); // Füge die Navbar hinzu
 
     mobileMenu.addEventListener("click", function () {
         console.log("Hamburger-Menü wurde geklickt!");
         navList.classList.toggle("active");
-        mobileMenu.classList.toggle("open"); /* Fügt die Animation zum Hamburger hinzu */
+        mobileMenu.classList.toggle("open"); 
+        navbar.classList.toggle("menu-open"); // Neue Klasse für Farbwechsel hinzufügen
     });
 });
+
 
